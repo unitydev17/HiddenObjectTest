39f2ac620fa04be9a7eb8e2faf692c9baeb030b5
Alexey Zhardzetski <unitydev17@gmail.clom>
2024-06-03 14:01:16 +0300

    Cache memory fix

--- Assets/_Project/Code/Services/Infrastructure/CacheService.cs
+++ Assets/_Project/Code/Services/Infrastructure/CacheService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
@@ -5,12 +6,19 @@ namespace Code.Services
 {
     public class CacheService : ICacheService
     {
+        
+        private readonly Dictionary<string, Texture2D> _memoryCache = new Dictionary<string, Texture2D>();
         public Texture2D LoadFromCache(string path)
         {
+            if (_memoryCache.ContainsKey(path)) return _memoryCache.GetValueOrDefault(path);
+            
             var bytes = File.ReadAllBytes(path);
             var texture = new Texture2D(2, 2);
             texture.LoadImage(bytes);
             texture.Apply();
+            
+            _memoryCache.Add(path, texture);
+            
             return texture;
         }
 
